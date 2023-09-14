feb02935c58431e63fbe77c1a609a1c982fb6867
Jan <yansemashchuk@gmail.com>
2023-09-14 23:35:16 +0200

    Update main.tf

--- IaC/main.tf
+++ IaC/main.tf
@@ -1,17 +1,13 @@
-resource "random_pet" "rg_name" {
-  prefix = var.resource_group_name_prefix
-}
-
 resource "azurerm_resource_group" "rg" {
   location = var.resource_group_location
-  name     = random_pet.rg_name.id
+  name     = "NSSFMsRG-smooth-sloth"
 }
 
-resource "random_pet" "azurerm_kubernetes_cluster_name" {
-  prefix = "cluster"
+resource "azurerm_kubernetes_cluster_name" {
+  name = "cluster-current-ladybird"
 }
 
-resource "random_pet" "azurerm_kubernetes_cluster_dns_prefix" {
+resource "azurerm_kubernetes_cluster_dns_prefix" {
   prefix = "dns"
 }
 
@@ -26,9 +22,9 @@ resource "azurerm_storage_account" "nssfmstorage" {
 
 resource "azurerm_kubernetes_cluster" "k8s" {
   location            = azurerm_resource_group.rg.location
-  name                = random_pet.azurerm_kubernetes_cluster_name.id
+  name                = azurerm_kubernetes_cluster_name.id
   resource_group_name = azurerm_resource_group.rg.name
-  dns_prefix          = random_pet.azurerm_kubernetes_cluster_dns_prefix.id
+  dns_prefix          = azurerm_kubernetes_cluster_dns_prefix.id
 
   identity {
     type = "SystemAssigned"
