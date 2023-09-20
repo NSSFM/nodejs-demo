3a03206f18bcd2f15add682cc9cd44bfc09c3950
Jan <yansemashchuk@gmail.com>
2023-09-20 17:00:54 +0200

    Update outputs.tf

--- IaC/outputs.tf
+++ IaC/outputs.tf
@@ -8,35 +8,35 @@ output "kubernetes_cluster_name" {
 
 output "client_certificate" {
   value     = azurerm_kubernetes_cluster.k8s.kube_config[0].client_certificate
-  sensitive = nonsensitive
+  sensitive = true
 }
 
 output "client_key" {
   value     = azurerm_kubernetes_cluster.k8s.kube_config[0].client_key
-  sensitive = nonsensitive
+  sensitive = true
 }
 
 output "cluster_ca_certificate" {
   value     = azurerm_kubernetes_cluster.k8s.kube_config[0].cluster_ca_certificate
-  sensitive = nonsensitive
+  sensitive = true
 }
 
 output "cluster_password" {
   value     = azurerm_kubernetes_cluster.k8s.kube_config[0].password
-  sensitive = nonsensitive
+  sensitive = true
 }
 
 output "cluster_username" {
   value     = azurerm_kubernetes_cluster.k8s.kube_config[0].username
-  sensitive = nonsensitive
+  sensitive = true
 }
 
 output "host" {
   value     = azurerm_kubernetes_cluster.k8s.kube_config[0].host
-  sensitive = nonsensitive
+  sensitive = true
 }
 
 output "kube_config" {
   value     = azurerm_kubernetes_cluster.k8s.kube_config_raw
-  sensitive = nonsensitive
+  sensitive = true
 }
