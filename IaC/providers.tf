696123d3904a265934dd75c30d3c7c8b1f7c8bd7
Jan <yansemashchuk@gmail.com>
2023-11-09 17:28:42 +0100

    Update providers.tf

--- IaC/providers.tf
+++ IaC/providers.tf
@@ -20,6 +20,13 @@ terraform {
     }
   }
 
+  backend "azurerm" {
+      resource_group_name   = "NSSFMs-storage-rg"
+      storage_account_name   = "nssfmstorage"
+      container_name         = "tf-state"
+      key                    = "tf/terraform.tfstate"
+    }
+
 }
 
 provider "azurerm" {
