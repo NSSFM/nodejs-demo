3589cef6eda305b71cca2b942683c5a71f2321ce
Jan <yansemashchuk@gmail.com>
2023-11-09 17:08:08 +0100

    Update main.tf

--- IaC/main.tf
+++ IaC/main.tf
@@ -3,14 +3,6 @@ resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
 }
 
-resource "azurerm_storage_account" "nssfmstorage" {
-  name                     = "nssfmstorage"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  account_kind             = "StorageV2"
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
 
 resource "azurerm_kubernetes_cluster" "k8s" {
   location            = azurerm_resource_group.rg.location
