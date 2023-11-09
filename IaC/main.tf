c1e00b319cba7cc594ee59c4aa969292d8fdc95a
Jan <yansemashchuk@gmail.com>
2023-11-09 14:23:57 +0100

    Update main.tf

--- IaC/main.tf
+++ IaC/main.tf
@@ -41,3 +41,12 @@ resource "azurerm_kubernetes_cluster" "k8s" {
     load_balancer_sku = "standard"
   }
 }
+
+  terraform {
+    backend "azurerm" {
+      resource_group_name   = azurerm_resource_group.rg.name
+      storage_account_name  = azurerm_storage_account.nssfmstorage.name
+      container_name        = "your-container"
+      key                   = "terraform.tfstate"
+  }
+}
