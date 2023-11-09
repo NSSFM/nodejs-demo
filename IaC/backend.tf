10bbf938d8ba1b8a642a7586cf07ac65e035a13c
Jan <yansemashchuk@gmail.com>
2023-11-09 15:19:17 +0100

    Update backend.tf

--- IaC/backend.tf
+++ IaC/backend.tf
@@ -1,7 +1,7 @@
 terraform { 
   backend "azurerm" {
       resource_group_name   = NSSFMsRG-smooth-sloth
-      storage_account_name   = azurerm_storage_account.nssfmstorage.name
+      storage_account_name   = nssfmstorage
       container_name         = azurerm_storage_container.tf_state_container.name
       key                    = "tf/terraform.tfstate"
     }
