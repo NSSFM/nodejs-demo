4b4d01f240ab70d93ef00242116e1988427ee3aa
Jan <yansemashchuk@gmail.com>
2023-11-09 15:18:45 +0100

    Update providers.tf

--- IaC/providers.tf
+++ IaC/providers.tf
@@ -19,12 +19,6 @@ terraform {
       version = "0.9.1"
     }
   }
-  backend "azurerm" {
-    resource_group_name   = azurerm_resource_group.rg.name
-    storage_account_name   = azurerm_storage_account.nssfmstorage.name
-    container_name         = azurerm_storage_container.tf_state_container.name
-    key                    = "tf/terraform.tfstate"
-  }
 
 }
 
