b56f8ac8a36aa3e975a86f4ee24ec2411368ec74
Jan <yansemashchuk@gmail.com>
2023-11-09 17:07:41 +0100

    Delete IaC-Backend directory

--- IaC-Backend/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name   = "NSSFMsRG-smooth-sloth"
-    storage_account_name   = "nssfmstorage"
-    container_name         = "tfstate-container"
-    key                    = "terraform.tfstate"
-    access_key             = "vqtF/6Y/ac/y9Krpxj9tKdUYCFmLD7nMdK0ioCoyrJuExk7KGLPvGAfOgl5FRickmAaxlMzHpMuW+AStypTweQ==" 
-  }
-}
